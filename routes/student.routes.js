53a06b8cf10d536fe2405b18bd28e77fc52744dc
NileshDeshmukh09 <nileshdeshmukh0908@gmail.com>
2023-03-11 23:53:23 +0530

    Added Authorization to routes

--- routes/student.routes.js
+++ routes/student.routes.js
@@ -4,15 +4,17 @@ const upload = require('../middlewares/multer');
 const validateCsv = require('../middlewares/validate-csv');
 const handleDbError = require('../middlewares/handle-db-error');
 const studentController = require('../controllers/student.controller');
+const  JWTAuth = require("../middlewares/JWTAuth");
+
 
 // upload data of CSV to db
-router.post('/students/upload', upload.single('file'), validateCsv, studentController.addStudentsFromCSV , handleDbError);
+router.post('/students/upload', JWTAuth.verifyToken , upload.single('file'), validateCsv, studentController.addStudentsFromCSV , handleDbError);
 
 // GET all students
-router.get('/students', studentController.getAllStudents , handleDbError);
+router.get('/students', JWTAuth.verifyToken ,  studentController.getAllStudents , handleDbError);
 
 // Export Data
-router.get('/students/export', studentController.exportData );
+router.get('/students/export', JWTAuth.verifyToken , studentController.exportData );
 
 
 module.exports = router;
