c4b31d910406e4cecd7cf56763584825c1947292
NileshDeshmukh09 <nileshdeshmukh0908@gmail.com>
2023-03-11 09:33:08 +0530

    connected sequalize to express'

--- index.js
+++ index.js
@@ -2,13 +2,25 @@ const express =require("express");
 const logger = require("morgan");
 const bodyParser = require("body-parser");
 const dotenv =  require("dotenv")
-dotenv.config();
-
 const app = express();
+dotenv.config();
+const Sequelize = require('sequelize');
 
 app.use(logger('dev'))
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
+
+const sequelize = new Sequelize('studentdb', 'root', '1234', {
+  host: 'localhost',
+  dialect: 'mysql'
+});
+
+sequelize.authenticate()
+  .then(() => console.log('Connection has been established successfully.'))
+  .catch(err => console.error('Unable to connect to the database :', err));
+
+
+
 
 
 app.listen(process.env.PORT , () => {
